9081d2f8b556469a95049ee6108cfaf40819fac8
jcmartinez <jcmartinezcu@gmail.com>
2020-06-01 16:26:48 +0200

    inicio proyecto

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Bienvenido a Angular';
+
+  curso = 'curso Spring 5 con Angular 7';
+  profesor:string = 'Andrés Guzman';
+}

11eadcb5a605852f77a979e171ae27ce4525014b
jcmartinez <jcmartinezcu@gmail.com>
2020-06-02 16:59:28 +0200

    video 303

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { CLIENTES } from './clientes.json';
 
 @Component({
   selector: 'app-clientes',
@@ -7,24 +8,12 @@ import { Cliente } from './cliente';
 })
 export class ClientesComponent implements OnInit {
 
-  clientes: Cliente []= [
-    {id:1, nombre:'Andres',apellido:'Guzman',createAt:'2017-11-12',email:'profesor@bolsaideas.com'},
-    {id:2, nombre:'Andres2',apellido:'Guzman2',createAt:'2017-11-13',email:'profeso1r@bolsaideas.com'},
-    {id:3, nombre:'Andres3',apellido:'Guzman3',createAt:'2017-11-14',email:'profesor2@bolsaideas.com'},
-    {id:4, nombre:'Andres4',apellido:'Guzman4',createAt:'2017-11-15',email:'profesor3@bolsaideas.com'},
-    {id:5, nombre:'Andres5',apellido:'Guzman5',createAt:'2017-11-16',email:'profesor4@bolsaideas.com'},
-    {id:6, nombre:'Andres6',apellido:'Guzman6',createAt:'2017-11-17',email:'profesor5@bolsaideas.com'},
-    {id:7, nombre:'Andres7',apellido:'Guzman7',createAt:'2017-11-18',email:'profesor6@bolsaideas.com'},
-    {id:8, nombre:'Andres8',apellido:'Guzman8',createAt:'2017-11-18',email:'profesor7@bolsaideas.com'},
-    {id:9, nombre:'Andres9',apellido:'Guzman9',createAt:'2017-11-2',email:'profesor8@bolsaideas.com'},
-    {id:10, nombre:'Andres10',apellido:'Guzman10',createAt:'2017-11-3',email:'profesor9@bolsaideas.com'},
-    {id:11, nombre:'Andres11',apellido:'Guzman11',createAt:'2017-11-4',email:'profesor10@bolsaideas.com'}
- 
-  ]
+  clientes: Cliente [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.clientes = CLIENTES;
   }
 
 }

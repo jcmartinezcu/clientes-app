4b4f804351a78803a30808abcbb945c3fa9b5df1
jcmartinez <jcmartinezcu@gmail.com>
2020-06-02 14:32:12 +0200

    video 299

---
+++ src/app/directiva/directiva.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html'
+})
+export class DirectivaComponent  {
+
+  listaCurso: String[] = ['TypeScript', 'JavaScript', 'Java SE', 'C#', 'PHP'];
+  constructor() { }
+
+  
+
+}
